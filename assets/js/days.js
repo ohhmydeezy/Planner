783c5350c2dffab751afe01ede53a3f090bc4481
OhhMyDeezy <ohhmydeezy@MacDeezy.ultrahub>
2024-01-31 22:45:06 +0000

    fixed storage function on click

--- assets/js/days.js
+++ assets/js/days.js
@@ -1,58 +1,49 @@
 // variable to create and loop through scheduler
 
-var myWorkDay = [
+let myWorkDay = [
     {
         id: "0",
         hour: "09",
-        ampm: "am",
         reminder: ""
     },
     {
         id: "1",
         hour: "10",
-        ampm: "am",
         reminder: ""
     },
     {
         id: "2",
         hour: "11",
-        ampm: "am",
         reminder: ""
     },
     {
         id: "3",
         hour: "12",
-        ampm: "pm",
         reminder: ""
     },
     {
         id: "4",
-        hour: "01",
-        ampm: "pm",
+        hour: "13",
         reminder: ""
     },
     {
         id: "5",
-        hour: "02",
-        ampm: "pm",
+        hour: "14",
         reminder: ""
     },
     {
         id: "6",
-        hour: "03",
-        ampm: "pm",
+        hour: "15",
         reminder: ""
     },
     {
         id: "7",
-        hour: "04",
-        ampm: "pm",
+        hour: "16",
         reminder: ""
     },
     {
         id: "8",
-        hour: "05",
-        ampm: "pm",
+        hour: "17",
         reminder: ""
     }
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-
 var timeblocksContainer = $("#time-blocks")
 var timeblocksContainer = document.querySelector(".container")
 var now = dayjs().format("HH");
-var saveBtn = $(".saveBtn")
+var saveBtn = $(".saveBtn");
+
 
 //display current date and time
 
@@ -11,64 +11,6 @@ var dateTime = $(document).ready(function () {
     $("#date-time").text("Date: " + today.format("ddd D MMM YYYY, HH:mm"));
 });
 
-// create timeblocks
-
-var myWorkDay = [
-    {
-        id: "0",
-        hour: "9",
-        ampm: " am",
-        reminder: ""
-    },
-    {
-        id: "1",
-        hour: "10",
-        ampm: " am",
-        reminder: ""
-    },
-    {
-        id: "2",
-        hour: "11",
-        ampm: " am",
-        reminder: ""
-    },
-    {
-        id: "3",
-        hour: "12",
-        ampm: " pm",
-        reminder: ""
-    },
-    {
-        id: "4",
-        hour: "1",
-        ampm: " pm",
-        reminder: ""
-    },
-    {
-        id: "5",
-        hour: "2",
-        ampm: "pm",
-        reminder: ""
-    },
-    {
-        id: "6",
-        hour: "3",
-        ampm: " pm",
-        reminder: ""
-    },
-    {
-        id: "7",
-        hour: "4",
-        ampm: " pm",
-        reminder: ""
-    },
-    {
-        id: "8",
-        hour: "5",
-        ampm: " pm",
-        reminder: ""
-    }
-];
 
 for (var i = 0; i < myWorkDay.length; i++) {
     var timeBlock = myWorkDay[i];
@@ -76,52 +18,55 @@ for (var i = 0; i < myWorkDay.length; i++) {
 
 // add timeblock container to html
 myWorkDay.forEach(function (timeBlock) {
-    var timeLabel = timeBlock.hour + timeBlock.ampm;
+    var timeLabel = timeBlock.hour;
     var blockColor = colorRow(timeLabel);
-    var row =
-        '<div class="time-block" id="' +
-        '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-        timeLabel +
-        '</div><textarea class="form-control ' +
-        blockColor +
-        '">' +
-        timeBlock.reminder +
-        '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
-
+    var row = `
+    <div>
+        <div class="row no-gutters input-group">
+            <div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">
+                ${timeLabel}
+            </div>
+            <textarea class="form-control time-block">
+                ${blockColor}
+                ${timeBlock.reminder} 
+            </textarea>
+            <button class="saveBtn btn-block" type="submit">
+                <i class="fas fa-save"></i>
+            </button>
+        </div>
+    </div>;
+`
     /* Adding rows to container div */
     $(".container").append(row);
 
 
 });
 function colorRow(time) {
-    var planNow = dayjs().format("HH");
-    var planEntry = dayjs(time, "HH").format("HH");
-    console.log(planNow);
-    if (planNow > planEntry) {
-        return "past";
-    } else if (planNow < planEntry) {
+    var planNow = now;
+    var planEntry = time;
+
+    if (planNow < (planEntry)) {
         return "future";
+    } else if (planNow > (planEntry)) {
+        return "past";
     } else {
-    return "present";
+        return "present";
     }
-
-
 }
-
 // save schedule
 
-$("saveBtn").on("click", function () {
-    var planEntry = $(this).siblings("textarea").val();
-    localStorage.setItem("textarea", JSON.stringify(planEntry));
+$(".saveBtn").on("click", function () {
+    var userInput = $(this).siblings(".time-block").val();
+    var hourSpan = $(this).siblings(".hour").text();
+    window.localStorage.setItem(hourSpan, userInput);
 });
-
 // clear schedule
 function clearScreen() {
     // Clear the options
-    scheduleInput.innerHTML = "";
+    scheduleInput.textContent = "";
 }
 
 $("#clearBtn").on("click", function () {
-    localStorage.clear();
+    window.localStorage.clear();
     clearScreen();
 });
